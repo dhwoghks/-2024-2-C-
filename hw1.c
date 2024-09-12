25fb784343df88a72e6d27af8b36301b0bb9db0b
Oh jaehwan <111697672+dhwoghks@users.noreply.github.com>
2024-09-12 16:58:08 +0900

    Create hw1.c
    숙제

---
+++ hw1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int n1, n2;
+	printf("Input two integers: ");
+	scanf_s("%d %d", &n1, &n2);
+	printf("%d & %d = %d\n", n1, n2, n1 & n2);
+	printf("%d | %d = %d\n", n1, n2, n1 | n2);
+	printf("%d ^ %d = %d\n", n1, n2, n1 ^ n2);
+	return 0;
+}		
